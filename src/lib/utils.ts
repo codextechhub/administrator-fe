6136704928094f01eb29b37f9f0a4d25694bc177
Emydev-op <alphaoseghe@gmail.com>
2026-03-26 17:05:55 +0100

    added vercel

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,15 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export function getInitials(name: string): string {
+  if (!name) return "";
+
+  const words = name.trim().split(/\s+/);
+  const initials = words
+    .slice(0, 2) // Limit to first 2 words
+    .map((word) => word[0].toUpperCase())
+    .join("");
+
+  return initials;
+}
